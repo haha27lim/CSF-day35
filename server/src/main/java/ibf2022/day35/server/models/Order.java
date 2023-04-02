3f1470e501a89405332d3e3f9be067bc37d8a468
haha27lim <eileen2708@hotmail.com>
2023-04-02 20:10:05 +0800

    updated

--- server/src/main/java/ibf2022/day35/server/models/Order.java
+++ server/src/main/java/ibf2022/day35/server/models/Order.java
@@ -30,11 +30,14 @@ public class Order {
 	public void addLineItem(LineItem lineItem) { this.lineItems.add(lineItem); }
 	public void removeLineItem(int idx) { this.lineItems.remove(idx); }
 
+	// Method to convert the object to a JSON object
 	public JsonObject toJson() {
+		// Create a JsonArrayBuilder to hold the line items
 		JsonArrayBuilder arrBuilder = Json.createArrayBuilder();
+		// Convert each line item in the list to a JSON object and add it to the JsonArrayBuilder
 		lineItems.stream()
 			.forEach(v -> arrBuilder.add(v.toJson()));
-
+		// Create a JsonObjectBuilder with the order details and the JsonArrayBuilder of line items
 		return Json.createObjectBuilder()
 			.add("orderId", orderId)
 			.add("name", name)
@@ -43,16 +46,24 @@ public class Order {
 			.build();
 	}
 
+	// Method to convert a JSON string to an Order object
 	public static Order toOrder(String j) {
+		// Create a JsonReader to read the JSON string
 		JsonReader reader = Json.createReader(new StringReader(j));
+		// Call the toOrder method with the read JsonObject
 		return toOrder(reader.readObject());
 	}
+
+	// Method to convert a JsonObject to an Order object
 	public static Order toOrder(JsonObject j) {
+		// Create a new Order object
 		Order order = new Order();
+		// Set the order ID if it is present in the JsonObject
 		if (j.containsKey("orderId") && (!j.isNull("orderId")))
 			order.setOrderId(j.getString("orderId"));
 		order.setName(j.getString("name"));
 		order.setEmail(j.getString("email"));
+		// Convert the line items from the JsonObject's JsonArray to a list of LineItem objects and set it in the Order object
 		List<LineItem> lineItems = j.getJsonArray("lineItems").stream()
 			.map(i -> i.asJsonObject())
 			.map(LineItem::toLineItem)
